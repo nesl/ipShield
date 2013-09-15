03f365e455c274bee2b1b4472eef9e945906cb86
Chenguang Shen <pscggeforce@gmail.com>
2013-09-15 13:34:59 -0700

    remove SensorDump

--- SensorDump/gen/edu/ucla/ee/nesl/privacyfilter/sensordump/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.ucla.ee.nesl.privacyfilter.sensordump;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
