00b909f139e3bdd7d97c34ad3de5fda7ba112a1b
Chenguang Shen <pscggeforce@gmail.com>
2013-10-04 11:50:13 -0700

    add content to inference database

--- javascripts/datatable.js
+++ javascripts/datatable.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
-    $('#example').dataTable( {
+    $('#data').dataTable( {
         "aaData": [
-            /* Reduced data set */
-            [ "Trident", "Internet Explorer 4.0", "Win 95+", 4, "X" ],
-            [ "Trident", "Internet Explorer 5.0", "Win 95+", 5, "C" ],
-            [ "Trident", "Internet Explorer 5.5", "Win 95+", 5.5, "A" ],
-            [ "Trident", "Internet Explorer 6.0", "Win 98+", 6, "A" ],
-            [ "Trident", "Internet Explorer 7.0", "Win XP SP2+", 7, "A" ],
-            [ "Gecko", "Firefox 1.5", "Win 98+ / OSX.2+", 1.8, "A" ],
-            [ "Gecko", "Firefox 2", "Win 98+ / OSX.2+", 1.8, "A" ],
-            [ "Gecko", "Firefox 3", "Win 2k+ / OSX.3+", 1.9, "A" ],
-            [ "Webkit", "Safari 1.2", "OSX.3", 125.5, "A" ],
-            [ "Webkit", "Safari 1.3", "OSX.3", 312.8, "A" ],
-            [ "Webkit", "Safari 2.0", "OSX.4+", 419.3, "A" ],
-            [ "Webkit", "Safari 3.0", "OSX.4+", 522.1, "A" ]
+            [ "Accelerometer", "Transportation mode", "still, walk, run", "93.6%", "[1]" ],
+            [ "Accelerometer, GPS", "Transportation mode", "still, walk, run, drive, bike", "93.6%", "[1]" ],
+            [ "Accelerometer, Gyroscope", "Onscreen Taps", "Location of taps on phone screen", "90%", "[2]" ],
+            [ "Accelerometer, Gyroscope", "Onscreen Taps", "Text entered on phone", "80%", "[2]" ],
+            [ "Accelerometer", "Device placement", "bag, ear, hand, pocket", "99.6%", "[3]" ],
+            [ "Accelerometer, Microphone, Bluetooth", "Emotion", "happy, sad, fear, anger, neutral", "--", "[4]" ],
         ],
         "aoColumns": [
-            { "sTitle": "Engine" },
-            { "sTitle": "Browser" },
-            { "sTitle": "Platform" },
-            { "sTitle": "Version", "sClass": "center" },
-            { "sTitle": "Grade", "sClass": "center" }
+            { "sTitle": "Sensor" },
+            { "sTitle": "Inference Category" },
+            { "sTitle": "Context" },
+            { "sTitle": "Accuracy"},
+            { "sTitle": "Reference"}
         ]
     } );   
+
+    $('#ref').dataTable( {
+        "aaData": [
+            [ "[1]", "Using Mobile Phones to Determine Transportation Modes", "ACM TOSN" ],
+            [ "[2]", "TapPrints: Your Finger Taps Have Fingerprints", "MobiSys'13" ],
+            [ "[3]", "Online Pose Classiﬁcation and Walking Speed Estimation using Handheld Devices", "UbiComp'12" ],
+            [ "[4]", "EmotionSense: a mobile phones based adaptive platform for experimental social psychology research", "UbiComp'10" ]
+        ],
+        "aoColumns": [
+            { "sTitle": "No." },
+            { "sTitle": "Name" },
+            { "sTitle": "Venue" }
+        ]
+    } ); 
 } );
